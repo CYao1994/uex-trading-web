b3c4899f126ba79f555bc2ea3c4ea04e3b053c96
CYao1994 <CYao1994@users.noreply.github.com>
2026-06-16 12:39:21 +0800

    Revert "debug: 添加terminal-fields调试端点+edge function"
    This reverts commit 156dc9d19acaffc143a63aace5c87bb7e65ab19c.

--- edge-functions/api/debug/terminal-fields.js
+++
@@ -1,6 +0,0 @@
-import { passthroughToBackend } from '../_shared/cache-helper.js';
-
-export async function onRequestGet(ctx) {
-  const url = new URL(ctx.request.url);
-  return passthroughToBackend(ctx.request, '/debug/terminal-fields', url);
-}
